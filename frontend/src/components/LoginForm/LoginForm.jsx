8239b7bce5e5c561cbde6fd805953e71759f7890
Taimoor Zaeem <mtaimoorzaeem@gmail.com>
2022-05-29 14:56:39 +0500

    Added form validation to login and signup pages.

--- frontend/src/components/LoginForm/LoginForm.jsx
+++ frontend/src/components/LoginForm/LoginForm.jsx
@@ -3,6 +3,11 @@ import axios from 'axios';
 import Classes from "./LoginForm.css";
 
 class LoginForm extends Component {
+
+  state = {
+    errors: {}
+  };
+
   handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -17,7 +22,9 @@ class LoginForm extends Component {
       window.location = '/';
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
-        console.log(ex.response.data);
+        const { errors } = {...this.state};
+        errors.message = ex.response.data;
+        this.setState({ errors });
       }
     }
     
@@ -29,30 +36,33 @@ class LoginForm extends Component {
       <div className={Classes.loginFormContainer}>
         <h1 className={Classes.loginH1}>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className={Classes.loginFormGroup}>
-            <input 
-                id="email"
-                name="email"
-                type="email" 
-                className={Classes.loginFormInput} 
-                placeholder="Email" 
-            />
+          <div className={Classes.loginFormElements}>
+            <div className={Classes.loginFormGroup}>
+              <input 
+                  id="email"
+                  name="email"
+                  type="email" 
+                  className={Classes.loginFormInput} 
+                  placeholder="Email" 
+              />
+            </div>
+            <div className={Classes.loginFormGroup}>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                className={Classes.loginFormInput}
+                placeholder="Password"
+              />
+            </div>
+            {this.state.errors.message && <p className={Classes.loginError}>{this.state.errors.message}</p>}
+            <div class={Classes.loginButtonAnker}>
+              <button className={Classes.loginButton}>Login</button>
+              <span>
+                Don't have an account? <a className={Classes.loginAnker} href="/Signup">Signup Now!</a>
+              </span>
+            </div>
           </div>
-          <div className={Classes.loginFormGroup}>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              className={Classes.loginFormInput}
-              placeholder="Password"
-            />
-          </div>
-          <div>
-            <button className={Classes.loginButton}>Login</button>
-          </div>
-          <span>
-            Don't have an account? <a href="/Signup">Signup Now!</a>
-          </span>
         </form>
       </div>
     );
--- frontend/src/components/SignupForm/SignupForm.jsx
+++ frontend/src/components/SignupForm/SignupForm.jsx
@@ -3,6 +3,12 @@ import axios from "axios";
 import Classes from "./SignupForm.css";
 
 class SignupForm extends Component {
+  
+  state = {
+    errors: {}
+  };
+
+
   handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -17,7 +23,11 @@ class SignupForm extends Component {
       localStorage.setItem('token', response.headers['x-auth-token']);
       window.location = '/';
     } catch (ex) {
-      console.log(ex.response.data);
+      if (ex.response && ex.response.status === 400) {
+        const { errors } = {...this.state};
+        errors.message = ex.response.data;
+        this.setState({ errors });
+      }
     }
     
   };
@@ -27,41 +37,42 @@ class SignupForm extends Component {
       <div className={Classes.signupFormContainer}>
         <h1 className={Classes.signupH1}>Sign Up</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className={Classes.formGroup}>
-            <input
-              name="name"
-              type="text"
-              className={Classes.signupFormInput}
-              placeholder="Name"
-            />
+          <div className={Classes.signupFormElements}>
+            <div className={Classes.formGroup}>
+              <input
+                name="name"
+                type="text"
+                className={Classes.signupFormInput}
+                placeholder="Name"
+              />
+            </div>
+            <div className={Classes.formGroup}>
+              <input
+                name="email"
+                type="email"
+                className={Classes.signupFormInput}
+                placeholder="Email"
+              />
+            </div>
+            <div className={Classes.formGroup}>
+              <input
+                name="password"
+                type="password"
+                className={Classes.signupFormInput}
+                placeholder="Password"
+              />
+            </div>
           </div>
-          <div className={Classes.formGroup}>
-            <input
-              name="email"
-              type="email"
-              className={Classes.signupFormInput}
-              placeholder="Email"
-            />
-          </div>
-          <div className={Classes.formGroup}>
-            <input
-              name="password"
-              type="password"
-              className={Classes.signupFormInput}
-              placeholder="Password"
-            />
-          </div>
+          {this.state.errors.message && <p className={Classes.signupError}>{this.state.errors.message}</p>}
           <input className={Classes.signupCheckbox} name="box" type="checkbox" />
           <span>
-            I agree to <a>terms and conditions.</a>
+            I agree to <a className={Classes.signupAnker} href="/">terms and conditions.</a>
           </span>
-          <br />
-          <br />
+          <br /><br/>
           <button className={Classes.signupButton}>Signup</button>
-          <br />
-          <br />
+          <br /><br/>
           <span>
-            Already have an account? <a href="/Login">Login Now!</a>
+            Already have an account? <a className={Classes.signupAnker} href="/Login">Login Now!</a>
           </span>
         </form>
       </div>
